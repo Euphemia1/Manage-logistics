3ea640205c9a09d6ff3e5a3824f12012c31a04c6
Euphemia1 <euphemiachikungulu347@gmail.com>
2025-02-05 09:17:36 +0200

    add new changes

--- Backend/get-jobs.php
+++ Backend/get-jobs.php
@@ -2,18 +2,20 @@
 header('Content-Type: application/json');
 
 // Database connection
-$conn = new mysqli('localhost', 
-'root',
- '',
- 'logistics');
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "logistics";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    echo json_encode(["error" => "Connection failed: " . $conn->connect_error]);
+    exit();
 }
 
 $result = $conn->query("SELECT * FROM jobs");
 $jobs = [];
-
 
 while ($row = $result->fetch_assoc()) {
     $jobs[] = $row;
