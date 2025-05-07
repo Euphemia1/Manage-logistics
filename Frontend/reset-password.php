9d808adfc037665b3033352397d1e87d06f05e38
Euphemia1 <euphemiachikungulu347@gmail.com>
2025-05-07 13:17:47 +0200

    add new chnages

--- Frontend/reset-password.php
+++ Frontend/reset-password.php
@@ -125,7 +125,7 @@ function validatePasswords() {
 }
 </script>
 
-<form action="" method="POST" id="resetForm">
+<form action="./Backend/reset-password.php" method="POST" id="resetForm">
     <!-- Change from process-reset.php to current file -->
     <!-- Ensure these hidden fields exactly match these names -->
     <input type="hidden" name="reset_token" value="<?= htmlspecialchars($token) ?>">
