8b722b7cb8987c197c57d879719fca5dd4488110
Euphemia1 <euphemiachikungulu347@gmail.com>
2025-05-08 11:44:57 +0200

    add chnages

--- Frontend/transporter-dashboard.php
+++ Frontend/transporter-dashboard.php
@@ -133,7 +133,7 @@ if (!isset($_SESSION['user_name'])) {
           </div>
           <div class="ml-4">
             <p class="text-sm font-medium text-gray-500">Active Trips</p>
-            <p class="text-2xl font-semibold text-gray-800">3</p>
+            <p class="text-2xl font-semibold text-gray-800"></p>
           </div>
         </div>
       </div>
