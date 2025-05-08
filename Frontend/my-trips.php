a6931bb444d7e8f73faaf2bad3ff7d3fe25e35e2
Euphemia1 <euphemiachikungulu347@gmail.com>
2025-05-08 12:06:24 +0200

    add chnages

--- Frontend/my-trips.php
+++ Frontend/my-trips.php
@@ -235,7 +235,7 @@ if (!isset($_SESSION['user_name'])) {
             <tr class="hover:bg-gray-50">
               <td class="py-3 px-4 text-sm text-gray-900"></td>
               <td class="py-3 px-4 text-sm text-gray-900"></td>
-              <td class="py-3 px-4 text-sm text-gray-900">i</td>
+              <td class="py-3 px-4 text-sm text-gray-900"></td>
               <td class="py-3 px-4 text-sm text-gray-900"></td>
               <td class="py-3 px-4 text-sm text-gray-900"></td>
               <td class="py-3 px-4 text-sm text-gray-900"></td>
