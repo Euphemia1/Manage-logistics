42650360cfd4b3c7bda282be20234c68af09a224
Euphemia1 <euphemiachikungulu347@gmail.com>
2025-05-08 12:03:21 +0200

    add chnages

--- Frontend/my-trips.php
+++ Frontend/my-trips.php
@@ -171,7 +171,7 @@ if (!isset($_SESSION['user_name'])) {
               <td class="py-3 px-4 text-sm text-gray-900"></td>
               <td class="py-3 px-4 text-sm">
                 <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-yellow-100 text-yellow-800">
-                  In Transit
+                  
                 </span>
               </td>
               <td class="py-3 px-4 text-sm text-gray-900"></td>
