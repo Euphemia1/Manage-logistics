64e14cdca7ff77bcd3448fb6c12048d47cda28e8
Euphemia1 <euphemiachikungulu347@gmail.com>
2025-07-04 12:11:22 +0200

    add chnages

---
+++ logistics (3).sql
@@ -0,0 +1,277 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 04, 2025 at 12:08 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `logistics`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cargo_owners`
+--
+
+CREATE TABLE `cargo_owners` (
+  `cargo_owner_id` int(11) NOT NULL,
+  `cargo_owner_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone_number` varchar(20) DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `company` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `cargo_owners`
+--
+
+INSERT INTO `cargo_owners` (`cargo_owner_id`, `cargo_owner_name`, `email`, `phone_number`, `password`, `company`) VALUES
+(1, 'Euphemia Chikungulu', 'euphemiachikungulu347@gmail.com', '+260762273483', '$2y$10$m9FUS89u3Y.VTk3br8/0FeW23ZNVVydJJrj84C11PnpDwJR.rzmDK', 'Spectrum Technologies limited');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jobs`
+--
+
+CREATE TABLE `jobs` (
+  `id` int(11) NOT NULL,
+  `item` varchar(255) NOT NULL,
+  `pickup` varchar(255) NOT NULL,
+  `dropoff` varchar(255) NOT NULL,
+  `weight` decimal(10,2) NOT NULL,
+  `status` varchar(100) DEFAULT NULL,
+  `start_date` date NOT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `jobs`
+--
+
+INSERT INTO `jobs` (`id`, `item`, `pickup`, `dropoff`, `weight`, `status`, `start_date`, `phone`, `created_at`) VALUES
+(15, 'Bagged Sulphur', 'Beira,Mozambique', 'Lubumbashi, DRC', 300.00, 'loading', '2025-06-24', '0972049151', '2025-06-24 08:58:49'),
+(16, 'Steel', 'lusaka', 'Harare, Zimbabwe', 600.00, 'Loading Ongoing', '2025-06-24', '0972049151', '2025-06-24 09:04:58');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notifications`
+--
+
+CREATE TABLE `notifications` (
+  `notification_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `cargo_owner_id` int(11) NOT NULL,
+  `message` text NOT NULL,
+  `status` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `order_id` int(11) NOT NULL,
+  `cargo_owner_id` int(11) NOT NULL,
+  `cargo_owner_name` varchar(255) NOT NULL,
+  `phone_number` varchar(20) DEFAULT NULL,
+  `cargo_type` varchar(100) DEFAULT NULL,
+  `weight` decimal(10,2) DEFAULT NULL,
+  `dimensions` varchar(100) DEFAULT NULL,
+  `origin` varchar(255) DEFAULT NULL,
+  `destination` varchar(255) DEFAULT NULL,
+  `instructions` text DEFAULT NULL,
+  `pickup_date` datetime DEFAULT NULL,
+  `status` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `token` varchar(64) NOT NULL,
+  `expires` datetime NOT NULL,
+  `user_type` enum('cargo_owners','transporters') NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transporters`
+--
+
+CREATE TABLE `transporters` (
+  `transporter_id` int(11) NOT NULL,
+  `transporter_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `phone_number` varchar(20) DEFAULT NULL,
+  `company` varchar(255) DEFAULT NULL,
+  `truck_number` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `transporters`
+--
+
+INSERT INTO `transporters` (`transporter_id`, `transporter_name`, `email`, `password`, `phone_number`, `company`, `truck_number`) VALUES
+(1, 'Euphemia Chikungulu', 'euphemiachikungulu347@gmail.com', '$2y$10$1mSaJa17hp7ft4CGoYY2AurWTU6ZbBzLqJv3SQOdX0Ju/tG6rRR3W', '+260762273483', 'Nyamuka Africa', NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indexes for table `cargo_owners`
+--
+ALTER TABLE `cargo_owners`
+  ADD PRIMARY KEY (`cargo_owner_id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `jobs`
+--
+ALTER TABLE `jobs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `notifications`
+--
+ALTER TABLE `notifications`
+  ADD PRIMARY KEY (`notification_id`),
+  ADD KEY `order_id` (`order_id`),
+  ADD KEY `cargo_owner_id` (`cargo_owner_id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_id`),
+  ADD KEY `cargo_owner_id` (`cargo_owner_id`);
+
+--
+-- Indexes for table `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `transporters`
+--
+ALTER TABLE `transporters`
+  ADD PRIMARY KEY (`transporter_id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `cargo_owners`
+--
+ALTER TABLE `cargo_owners`
+  MODIFY `cargo_owner_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `jobs`
+--
+ALTER TABLE `jobs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `notifications`
+--
+ALTER TABLE `notifications`
+  MODIFY `notification_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `password_resets`
+--
+ALTER TABLE `password_resets`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `transporters`
+--
+ALTER TABLE `transporters`
+  MODIFY `transporter_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `notifications`
+--
+ALTER TABLE `notifications`
+  ADD CONSTRAINT `notifications_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`),
+  ADD CONSTRAINT `notifications_ibfk_2` FOREIGN KEY (`cargo_owner_id`) REFERENCES `cargo_owners` (`cargo_owner_id`);
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`cargo_owner_id`) REFERENCES `cargo_owners` (`cargo_owner_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
