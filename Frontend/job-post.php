55bb57712cecb029bb21832c832c592a0080c621
Euphemia1 <euphemiachikungulu347@gmail.com>
2025-07-04 12:12:05 +0200

    add chnages

--- Frontend/job-post.php
+++ Frontend/job-post.php
@@ -2,6 +2,9 @@
 <!DOCTYPE html>
 <html lang="en">
 
+
+
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -692,3 +695,4 @@
 
 </body>
 </html>
+
