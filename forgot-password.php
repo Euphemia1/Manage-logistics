bfd9fafcd18fa01c75499869fa8209c84c7f1d36
Euphemia1 <euphemiachikungulu347@gmail.com>
2025-07-15 12:02:46 +0200

    add fogot password

--- forgot-password.php
+++ forgot-password.php
@@ -5,7 +5,7 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-require '../vendor/autoload.php';
+// git
 
 // Database connection setup
 $host = 'localhost';
