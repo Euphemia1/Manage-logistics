8801134615923b642c8caaae6fee14023752a149
Euphemia1 <euphemiachikungulu347@gmail.com>
2025-07-18 12:38:55 +0200

    add chnages

--- index.php
+++ index.php
@@ -673,7 +673,7 @@
             <div class="team-member">
                 <img src="./images/Faith.jpg" alt="" class="team-member-image">
                 <div class="team-member-info">
-                    <h3 class="team-member-name">Faith Mikumbiri</h3>
+                    <h3 class="team-member-name">Faith Mukumbiri</h3>
                     <p class="team-member-title">Chief Executive Officer</p>
                     <div class="team-social-links">
                         <a href="https://www.linkedin.com/in/faith-mukumbiri-909b01105?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" class="team-social-link" title="LinkedIn"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect x="2" y="9" width="4" height="10"></rect><circle cx="4" cy="4" r="2"></circle></svg></a>
@@ -717,7 +717,7 @@
                 <img src="./images/Mtaja.jpg" alt="" class="team-member-image">
                 <div class="team-member-info">
                     <h3 class="team-member-name">Faith Mtaja</h3>
-                    <p class="team-member-title">Startegic Assistant</p>
+                    <p class="team-member-title">Strategic Assistant</p>
                     <div class="team-social-links">
                         <a href="https://www.linkedin.com/in/faith-mtaja-a59968263?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" class="team-social-link" title="LinkedIn"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect x="2" y="9" width="4" height="12"></rect><circle cx="4" cy="4" r="2"></circle></svg></a>
                         <!-- <a href="#" class="team-social-link" title="Twitter"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z"></path></svg></a> -->
