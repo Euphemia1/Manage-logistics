4a36aaf8a50201fe4f56d266f4b2a4b66bae8305
Euphemia1 <euphemiachikungulu347@gmail.com>
2025-07-30 15:04:54 +0200

    add chnages

--- transporter-signup.php
+++ transporter-signup.php
@@ -6,23 +6,22 @@ $username = "root";
 $password = ""; 
 $dbname = "logistics";
 
-// Create connection
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Prepare and bind
+
 $stmt = $conn->prepare("INSERT INTO transporters (transporter_name, email, phone_number, password, company) VALUES (?, ?, ?, ?, ?)");
 $stmt->bind_param("sssss", $transporter_name, $email, $phone_number, $hashed_password, $company);
 
-// Check if form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Check if all required fields are set
+    
     if (isset($_POST['transporter_name'], $_POST['email'], $_POST['phone_number'], $_POST['password'], $_POST['company'])) {
-        // Set parameters
+        
         $transporter_name = trim($_POST['transporter_name']);
         $email = trim($_POST['email']);
         $phone_number = trim($_POST['phone_number']);
