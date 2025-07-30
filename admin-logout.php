24fc3565154ac378bce370c593a5fcd3b33a0d38
Euphemia1 <euphemiachikungulu347@gmail.com>
2025-07-30 15:31:57 +0200

    add chnages

--- admin-logout.php
+++ admin-logout.php
@@ -1,13 +1,9 @@
 <?php
-session_start(); // Start the session
+session_start(); 
 
-// Unset all session variables
 $_SESSION = [];
 
-// Destroy the session
 session_destroy();
-
-// Redirect to the index page
 header("index.php");
 exit();
 ?>
