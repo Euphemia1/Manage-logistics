68c168d17db73d3e9d836c5ce396ac5d29c27caa
Euphemia1 <euphemiachikungulu347@gmail.com>
2025-08-12 14:01:00 +0200

    add chnages

--- db.php
+++ db.php
@@ -1,11 +1,11 @@
 <?php
-// Database connection configuration
+
 $db_host = 'localhost';
 $db_user = 'u178619125_nyamula';
 $db_pass = '@BluDiamond0100';
 $db_name = 'u178619125_nyamula'; 
 
-// Create the database connection
+
 $conn = new mysqli($db_host, 
 $db_user,
  $db_pass, 
