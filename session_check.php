d29162401443b282bdcc4bcac7c15e611f92b81c
Euphemia1 <euphemiachikungulu347@gmail.com>
2025-08-12 14:07:15 +0200

    add chnages to codebase

--- session_check.php
+++ session_check.php
@@ -3,11 +3,8 @@ session_start();
 header('Content-Type: application/json');
 
 $response = ['expired' => false, 'redirect_url' => ''];
-
-// Set session timeout to 2 minutes (120 seconds)
 $session_timeout = 120;
 
-// Check if user is logged in and determine user type
 $user_type = '';
 $redirect_url = '';
 
@@ -23,7 +20,7 @@ if (isset($_SESSION['admin_id'])) {
         $redirect_url = 'transporter-login.php';
     }
 } elseif (isset($_SESSION['user_name'])) {
-    // For backward compatibility, check session variables to determine type
+
     if (isset($_SESSION['cargo_owner_id'])) {
         $user_type = 'cargo_owner';
         $redirect_url = 'cargo-owner-login.php';
@@ -33,14 +30,13 @@ if (isset($_SESSION['admin_id'])) {
     }
 }
 
-// If no valid session found, mark as expired
 if (empty($user_type)) {
     $response['expired'] = true;
     $response['redirect_url'] = 'index.php';
     session_unset();
     session_destroy();
 } else {
-    // Check if session has timed out
+   
     if (!isset($_SESSION['last_activity']) || 
         (time() - $_SESSION['last_activity'] > $session_timeout)) {
         $response['expired'] = true;
@@ -48,7 +44,7 @@ if (empty($user_type)) {
         session_unset();
         session_destroy();
     } else {
-        // Update last activity time
+     
         $_SESSION['last_activity'] = time();
     }
 }
