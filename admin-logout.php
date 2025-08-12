ec01b36b66a41c4542d801d9cd3f59eaa40214bc
Euphemia1 <euphemiachikungulu347@gmail.com>
2025-08-12 15:43:24 +0200

    add chnages

--- admin-logout.php
+++ admin-logout.php
@@ -1,13 +1,7 @@
 <?php
 session_start(); 
-
-// Clear all session variables
 $_SESSION = [];
-
-// Destroy the session
 session_destroy();
-
-// Redirect to admin login page
 header("Location: admin-login.php");
 exit();
 ?>
