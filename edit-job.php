384913ba796b1e4da2113d93db9054649cfa21ea
Euphemia1 <euphemiachikungulu347@gmail.com>
2025-08-19 16:05:07 +0200

    connect to db details

--- edit-job.php
+++ edit-job.php
@@ -1,9 +1,10 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "logistics";
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "logistics";
 
+require_once 'db.php';
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
